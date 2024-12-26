43256aecca2e7f265340b8b6c10239c6e3afbcbd
tanmay-12-ctrl <tanmayswain04@gmail.com>
2024-12-26 17:39:44 +0530

    changed client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,44 @@
 import React, { useState } from 'react';
-import Navbar from './components/Navbar';
-import AuthToggle from './components/AuthToggle'; // Import the AuthToggle component
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
-function App() {
-  const [isAuthVisible, setIsAuthVisible] = useState(false);
+import './index.css';
 
-  const toggleAuth = () => {
-    setIsAuthVisible(!isAuthVisible); // Toggle between showing and hiding Auth forms
+const App = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleRegisterClick = () => {
+    setIsActive(true);
+  };
+
+  const handleLoginClick = () => {
+    setIsActive(false);
   };
 
   return (
-    <div className="App">
-      <Navbar toggleAuth={toggleAuth} /> {/* Pass toggleAuth to Navbar */}
-      <h1>Welcome to the Authentication System</h1>
-      {isAuthVisible && <AuthToggle />} {/* Show AuthToggle based on the state */}
+    <div className={`container ${isActive ? 'active' : ''}`} id="container">
+      <Signup />
+      <Login handleLoginClick={handleLoginClick} />
+      <div className="toggle-container">
+        <div className="toggle">
+          <div className="toggle-panel toggle-left">
+            <h1>Welcome ZASHIONITES</h1>
+            <p>Enter your personal details to use all site features</p>
+            <button id="login" onClick={handleLoginClick}>
+              Sign In
+            </button>
+          </div>
+          <div className="toggle-panel toggle-right">
+            <h1>ZASHION</h1>
+            <p>Register with your personal details to use all site features</p>
+            <button id="register" onClick={handleRegisterClick}>
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 
-const Signup = () => {
+const Login = ({ handleLoginClick }) => {
   return (
-    <div className="form-container sign-up">
+    <div className="form-container sign-in">
       <form>
-        <h1>Create Account</h1>
+        <h1>Sign In</h1>
         <div className="social-icons">
           <a href="#" className="icon"><i className="fa-brands fa-google"></i></a>
           <a href="#" className="icon"><i className="fa-brands fa-facebook"></i></a>
           <a href="#" className="icon"><i className="fa-brands fa-github"></i></a>
           <a href="#" className="icon"><i className="fa-brands fa-linkedin"></i></a>
         </div>
-        <span> Use your email for registration</span>
-        <input type="text" placeholder="Name" />
+        <span> Use your email and password</span>
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
-        <button type="button">Sign Up</button>
+        <a href="#">Forgot your password?</a>
+        <button type="button">Sign In</button>
       </form>
     </div>
   );
 };
 
-export default Signup;
+export default Login;
